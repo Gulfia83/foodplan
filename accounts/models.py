0834a7511aa05bb57dc7292f89eff49be3e49157
a-skripko <skripkoae@gmail.com>
2024-10-24 15:39:22 +0400

    fix path to media folder with avatars

--- accounts/models.py
+++ accounts/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
-    avatar = models.ImageField(null=True, blank=True, upload_to='media/avatars')
+    avatar = models.ImageField(null=True, blank=True, upload_to='avatars')
     
     def str(self):
         return self.username
